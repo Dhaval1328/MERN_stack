90350b7b30c86dbb32704351fd3c8ba455082f97
Dhaval1328 <ddhaval2655@gmail.com>
2026-04-02 15:32:57 +0530

    Demo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import About from "./About";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
