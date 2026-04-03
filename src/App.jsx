89da140838d736f56f8dedb636abd625a85867f7
Dhaval1328 <ddhaval2655@gmail.com>
2026-04-03 10:33:09 +0530

    Router Use

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+// import About from "./About";
+// import Home from "./Home";
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//       <Home />
+//       <About />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import About from "./About";
 import Home from "./Home";
+import Contact from "./Contact";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <Home />
-      <About />
+
+      <Router>
+        <Link to="/home">Home</Link> | <Link to="/about">About</Link> |
+        <Link to="/contact">Contact</Link>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
